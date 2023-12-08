453a712fa509a1b300c31f2e07c1f2753d5255ed
unknown <vpaist@studio-tg.ru>
2023-12-08 16:56:54 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { isMobile, isDesktop } from 'react-device-detect';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isMobile ? (
+        <p>Устройство является мобильным</p>
+      ) : (
+        <p>Устройство является десктопом</p>
+      )}
+      {isDesktop ? <p>Режим десктопа</p> : <p>Режим мобильного устройства</p>}
     </div>
   );
 }
